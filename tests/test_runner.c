30671f0799d85e1e36830d961a8aedf52f43dc82
bwrob <bartosz.marcin.wroblewski@gmail.com>
2026-09-08 09:10:07 +0200

    Add snekobject field initialization test suite in tests/test_snekobject.c

--- tests/test_runner.c
+++ tests/test_runner.c
@@ -4,11 +4,13 @@
 extern MunitTest vm_tests[];
 extern MunitTest mark_tests[];
 extern MunitTest trace_tests[];
+extern MunitTest snekobject_tests[];
 
 static MunitSuite child_suites[] = {
     munit_suite("mark-and-sweep", vm_tests),
     munit_suite("mark", mark_tests),
     munit_suite("trace", trace_tests),
+    munit_suite("snekobject", snekobject_tests),
     { NULL, NULL, NULL, 0, MUNIT_SUITE_OPTION_NONE }
 };
 
---
+++ tests/test_snekobject.c
@@ -0,0 +1,33 @@
+#include "bootlib.h"
+#include "munit.h"
+#include "sneknew.h"
+#include "snekobject.h"
+#include "vm.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+munit_case(RUN, test_field_exists, {
+  vm_t *vm = vm_new();
+  snek_object_t *lane_courses = new_snek_integer(vm, 20);
+  snek_object_t *teej_courses = new_snek_integer(vm, 1);
+  (void)lane_courses->is_marked;
+  (void)teej_courses->is_marked;
+  vm_free(vm);
+  assert(boot_all_freed());
+});
+
+munit_case(SUBMIT, test_marked_is_false, {
+  vm_t *vm = vm_new();
+  snek_object_t *lane_courses = new_snek_integer(vm, 20);
+  snek_object_t *teej_courses = new_snek_integer(vm, 1);
+  assert_false(lane_courses->is_marked);
+  assert_false(teej_courses->is_marked);
+  vm_free(vm);
+  assert(boot_all_freed());
+});
+
+MunitTest snekobject_tests[] = {
+    munit_test("/field_exists", test_field_exists),
+    munit_test("/marked_is_false", test_marked_is_false),
+    munit_null_test,
+};
